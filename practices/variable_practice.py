74c05c047c9d350ee22bc7647c192508435ba829
jezux7 <jesus.durand@ucas-edu.net>
2025-08-29 14:42:40 -0600

    variiables notes

---
+++ practices/variable_practice.py
@@ -0,0 +1,10 @@
+# examples of variables
+School = "UCAS"
+single_digit = 4
+
+how_much = 10
+print(how_much)
+
+how_much = 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+
+print(how_much)
