3ed97d64a25a8fc53ef3969412a93abbfcbc1dc9
jezux7 <jesus.durand@ucas-edu.net>
2025-09-05 14:35:47 -0600

    expresion_notes

--- notes/expressions.py
+++ notes/expressions.py
@@ -1,7 +1,7 @@
 # what is an algorithm 
     # answer
-name = input("Whats your name")
-print("Hello",name)
+name = input("Whats your name ")
+print("Hello ",name)
 
 # Treyson has 12 apples, he has 5 frinds that he wants to give apples to. How many apples does each friend get? 
 apples = 12
@@ -21,16 +21,26 @@ pepole4 = 19
 print("The average of age is", (pepole1 + pepole2 + pepole3 + pepole4) /4)
 
 # List ALL of the different mathematical operators(symbol and what it does)
-num_one = 12
-num_two = 7
-print("Addition(+):", num_one+num_two)
-print("Subtraction(-):", num_one-num_two)
-print("Multipication(*):", num_one*num_two)
-print("Division(/):", num_one/num_two)
-print("Expomemts(**):", num_one**num_two)
-print("Integer division(//):", num_one//num_two)
-print("Modulo(%):", num_one%num_two)
+num_one = int(input("Tell a number:\n"))
+num_two = float(input("Tell me another number:\n"))
+num_one +=  num_two
+print("Addition(+):", num_one)
+num_one +=  num_two
+print("Subtraction(-):", num_one)
+num_one -=  num_two
+print("Multipication(*):", num_one)
+num_one *=  num_two
+print("Division(/):", round(num_one, 2)) # round(number to round, number of decimal places)
+num_one /=  num_two
+print("Expomemts(**):", num_one)
+num_one **=  num_two
+print("Integer division(//):", num_one)
+num_one //=  num_two
+print("Modulo(%):", num_one)
+num_one %=  num_two
 
 # integer division will only give ypu the whole numberwile dividing
 # floats in program are numbers with decimals
 # Modulo = what is left of the division 
+
+# We learn to use expresions for be able to create better codes for the goverment
---
+++ practices/buget_calculator.py
@@ -0,0 +1,15 @@
+# JD 7th Buget Calculator
+
+# I need to now
+# Rent
+# Utilities
+# Groceries
+# Transportation
+
+Income = input("How much money you got for moth")
+input("How much is your rent?")
+input("How much you pay in utilities?")
+input("How much you spent on groceries?")
+input("Gow much you pay in trasportation?")
+
+Spend = income -()
