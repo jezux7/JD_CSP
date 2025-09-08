d4e9a13c4eca9807fbf3f8bd2a4b7d084c5e048a
jezux7 <jesus.durand@ucas-edu.net>
2025-09-08 14:41:38 -0600

    stringnotes

--- notes/string.py
+++ notes/string.py
@@ -1,1 +1,34 @@
 # JD 7th String notes
+
+# What makes something a string?  
+# acollection of letter, numbers, and symbols that are surremdere by quatiation marks
+# Why do we have strings?
+
+# How do stupid proofing and sanitization relate to each other?
+
+# What is debugging?
+
+# How do you debug the different types of errors?
+
+# Describe what each of the methods below does:
+
+# find()
+# concatenate (add) the + is how you can combine to strings
+# upper()
+# lower()
+# strip() it get rid of space on the sides
+
+
+#Example
+
+first_name  = input("What your name:\n").strip().title()
+
+last_name = input("What your last name:\n").strip().title()
+
+full_name = first_name +  " " + last_name
+
+senetence = 'The quick brown fox jumps over the lazy dog'
+
+print(senetence.strip())
+
+print("Welcome to my progarm", last_name + ("!"))
