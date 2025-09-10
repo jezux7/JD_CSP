db4fb22594c2d50c7b5b4a095b9dda934b5f8ff8
jezux7 <jesus.durand@ucas-edu.net>
2025-09-10 14:36:43 -0600

    claswork

---
+++ practices/Name Decorator.py
@@ -0,0 +1,9 @@
+# JD 7th Name Decorator 
+
+name = input("Hi, tell me a name")
+
+decor1 = input("Put a sing going right:\n")
+
+decor2 = input("Enter another sing going left:\n")
+
+print(decor1 + name + decor2)
---
+++ practices/silly_sentences.py
@@ -0,0 +1,31 @@
+# JD 7th silly_sentences.py
+
+toys = input("Hi, tell me a toys")
+
+musical_instrument = input("Tell me a musical instrument")
+
+noun = input("Tell me a noun")
+
+desert = input("Tell me a desert")
+
+snack = input("Tell me a snack")
+
+verb= input("Tell me a verb")
+
+animals= input("Tell me a animals")
+
+noun2 = input("Tell me a noun")
+
+number = input("Tell me a number")
+
+noun3 = input("Tell me a noun")
+
+number2 = input("Tell me an other number")
+
+number3  = input("Tell me an other number")
+
+number4  = input("Tell me an other number")
+
+animals= input("Tell me an other animal")
+
+print()
