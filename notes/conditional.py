7185fce02292c24806d6aeca8dcde605c874f9bf
jezux7 <jesus.durand@ucas-edu.net>
2025-09-18 14:16:57 -0600

    notes

--- notes/conditional.py
+++ notes/conditional.py
@@ -12,7 +12,7 @@
 # What do else statements do?
     # Represent the other outcome, option, false
 # What kind of statement do you use if you have more than 2 needed outcomes?
-
+    #eilf
 # What do each of the different symbols mean in conditionals?
 # < Less than
 # > Greater than
@@ -22,11 +22,11 @@
 # ! Means Not
 
 # What are the 3 logical operators?
-
+    # And, Or, Not
 # What are logical operators for?
-
+    #they alwous to combine multiple conditions
 # What does a nested conditional statement do?
-
+    #only happends inside abother conditional
 
 
 #homework_done = input("Is your homework done? ").strip().capitalize()
@@ -39,12 +39,35 @@
 
 #grade = input("What is your grade ")
 
-grade = 100
+#grade = 72
+
+#if grade >= 90:
+    #print(f"You have {grade}% that is an A")
+#elif grade >= 80:
+    #print(f"You have {grade}% that is a B")
+#elif grade >= 70:
+    #print(f"You have a {grade}% that is a C or lower")
+
+
+name = input("What is your name: ")
+
+if name == "Ms LaRose":
+    print("you ate the teacher")
+elif name == "Tia" or name == "Ashley":
+    print("You are the TA")
+else:
+    print(f"hello {name}, you are a student")
+
+
+
+grade = 72
 
 if grade >= 90:
+    if grade is > 100:
+        print("You cheated thast false")
+    else:
     print(f"You have {grade}% that is an A")
 elif grade >= 80:
     print(f"You have {grade}% that is a B")
 elif grade >= 70:
-else:
     print(f"You have a {grade}% that is a C or lower")
