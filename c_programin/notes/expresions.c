90313e1b5f71b065a352b35a562920d850dd8ccf
jezux7 <jesus.durand@ucas-edu.net>
2025-10-08 14:30:26 -0600

    fjngchjg

--- c_programin/notes/expresions.c
+++ c_programin/notes/expresions.c
@@ -22,21 +22,21 @@ return 0;
 }
 
 // What is the difference between an integer and a float in C?
-  // an int) is a number without a decimal point.   a float us a floating-point num
+    // an int) is a number without a decimal ponit
 // How does C handle integer division compared to float division?
-  // integer division yields only the whole num quotient by truncating any decimal part
+    // 
 // List the arithmetic operators available in C and their functions.
-  // +, -, /, * and %
-// What is the modulus operator, and how is it used?
-  // a modulo its the remainder
-// How do you round a float to the nearest integer in C?
-  // .1
-// What is type casting in C? Provide an example of explicit type casting.
-  // The process of explicitily converting a variable of one data type to another by using the type cast operator
-// How do compound assignment operators work in C? List three examples.
-  // Addition, Equivalent, Multiplication
-// What is the purpose of the math.h library? Name three functions it provides.
-  // allow you to perform mathematical tasks on numbers
-// How do you print a float with a specific number of decimal places using printf()?
-  // %.NF
 
+// What is the modulus operator, and how is it used?
+
+// How do you round a float to the nearest integer in C?
+
+// What is type casting in C? Provide an example of explicit type casting.
+
+// How do compound assignment operators work in C? List three examples.
+
+// What is the purpose of the math.h library? Name three functions it provides.
+
+// How do you print a float with a specific number of decimal places using printf()?
+
+// What happens when you mix integer and float operations in C?
