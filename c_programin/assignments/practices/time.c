13b6b7bacc0f7416193ee530f85306173beed313
jezux7 <jesus.durand@ucas-edu.net>
2025-10-13 18:40:08 -0600

    Update time.c

--- c_programin/assignments/practices/time.c
+++ c_programin/assignments/practices/time.c
@@ -1,4 +1,22 @@
 // JD 7th Time of Day
 
 #include <stdio.h>
+#include <string.h>
 
+
+int main(void) {
+    int hour;
+
+    printf("What is the hour in military time: ");
+    scanf("%d", &hour);
+
+    if (hour < 12) {
+        printf("Good Morning!\n");
+    } else if (hour < 18) {
+        printf("Good Afternoon!\n");
+    } else {
+        printf("Good Evening!\n");
+    }
+
+    return 0;
+}
