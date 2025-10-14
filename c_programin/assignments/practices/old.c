28ea6ea55b302f9d74a3d1f2cb1bd9d871f82615
jezux7 <jesus.durand@ucas-edu.net>
2025-10-13 18:38:20 -0600

    Update old.c

--- c_programin/assignments/practices/old.c
+++ c_programin/assignments/practices/old.c
@@ -2,30 +2,20 @@
 
 #include <stdio.h>
 
-int main (void){
+int main(void) {
     int age;
-
-    printf("Hi, how old are you?: ");
+    printf("Enter your age: ");
     scanf("%d", &age);
-
-    if (age >= 18){
-        printf("you are old enough you can vote\n")
-    }
-    else if (age >= 16) {
-        printf("Youy are old enough to drive\n")
-    }
-    else if (age >= 16) {
-        printf("Youy are old enough to drive\n")
-    }
-    else if (age >= 16) {
-        printf("Youy are old enough to drive\n")
-    }
-    else if (age >= 15) {
-        printf("Youy are old enugh to get a learnes permit\n")
-        else if (age >= 16) {
-        printf("Youy are old enough to drive\n")
-    }
+    if (age >= 18) {
+        printf("You are old enough to vote.\n");
+    }else if (age >= 16);
+        printf("You are old enough to drive.\n");
+    } else if (age >= 15) {;
+        printf("You are old enough to get a learners permit.\n");
+    } else if (age >= 5) {
+        printf("You are old enough to go to school.\n");
+    } else {
+        printf("you aren't old enought to do anything do back to bed you baby.\n");
     }
     return 0;
-
 }
