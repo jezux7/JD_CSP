acb8fcba4d3d8a4e51dec0d2c736a257ab182a6b
jezux7 <jesus.durand@ucas-edu.net>
2025-10-14 14:10:02 -0600

    fizzc

---
+++ c_programin/notes/fuction.c
@@ -0,0 +1,20 @@
+// JD 7th Fuction notes
+
+// What a function is
+    // 
+// Why we use functions
+    // 
+// How to write a function in C
+    // 
+// What are arguments and parameters?
+    // 
+// How do arguments and parameters work together?
+    // 
+// How to use parameters and arguments in C
+    // 
+// What are return statements?
+    // 
+// How do return statements change how you define a function in C?
+    // 
+// What do return statements do with the information?
+    // 
