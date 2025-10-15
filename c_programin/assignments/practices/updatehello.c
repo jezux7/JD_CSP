5649675d5f3a463aebaf1679e0d76626eb72ce6d
jezux7 <jesus.durand@ucas-edu.net>
2025-10-14 19:09:49 -0600

    Update updatehello.c

--- c_programin/assignments/practices/updatehello.c
+++ c_programin/assignments/practices/updatehello.c
@@ -1,1 +1,18 @@
+// JD 7th Update Hello in C
 
+#include <stdio.h>
+
+
+void hello(char *name){
+    printf("Hello %s\n", name);
+}
+
+int main(void){
+    hello("Archi");
+    hello("Jinu");
+    hello("Manny");
+    hello("Isaiah");
+    hello("Jesus");
+
+    return 0;
+}
